449ac50ecf92ae00f4d0f9afd546275117ac3c9c
zhenhongswell <zhenhongswell@gmail.com>
2023-12-08 15:33:37 +0800

    add password error message

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-let passwordInput = document.querySelector('#password');
-let confirmPasswordInput = document.querySelector('#confirm-password');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+const passwordErrorMessage = document.querySelector('.sign-up-form-password-error-message');
 
 passwordInput.addEventListener("input",(event)=>{
-    checkPasswordIsConfirmed(passwordInput.value,confirmPasswordInput.value);
+    if (checkPasswordIsConfirmed(passwordInput.value,confirmPasswordInput.value) === false){
+        passwordErrorMessage.style.visibility = "visible";
+    }else{
+        passwordErrorMessage.style.visibility = "hidden";
+    }
 })
 
 confirmPasswordInput.addEventListener("input",(event)=>{
-    checkPasswordIsConfirmed(passwordInput.value,confirmPasswordInput.value);
+    if (checkPasswordIsConfirmed(passwordInput.value,confirmPasswordInput.value) === false){
+        passwordErrorMessage.style.visibility = "visible";
+    }else{
+        passwordErrorMessage.style.visibility = "hidden";
+    }
 })
 
 function checkPasswordIsConfirmed(password,confirmPassword){
     if (password === confirmPassword){
-        console.log('password is confirmed!');
-        return false
+        // console.log('password is confirmed!');
+        return true;
     }else{
-        console.log('password is not confirmed');
-        return true
+        // console.log('password is not confirmed');
+        return false;
     }
 }
-
-
